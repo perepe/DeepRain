f2c39f0acc4841c5b697ac288eafcb87c3a5f1a3
Pere Rajadell <pere.rajadell@hotmail.com>
2017-12-17 21:01:50 +0100

    optimize stream data management

--- DeepRain/DeepRain/Source/Stream.cpp
+++ DeepRain/DeepRain/Source/Stream.cpp
@@ -7,13 +7,11 @@ void Stream::add(float value)
 {
     if (_size < kStreamMaxSize)
     {
-        _data[_size] = value;
         ++_size;
-        return;
     }
 
-    memmove(&_data, &_data[1], sizeof(float) * kStreamMaxSize - 1);
-    _data[_size - 1] = value;
+    _last = (_last - 1 < 0 ? kStreamMaxSize : _last) - 1;
+    _data[_last] = value;
 }
 
 void Stream::draw(Allegro* allegro) const
@@ -28,7 +26,7 @@ void Stream::draw(Allegro* allegro) const
     float kMinValue = 99999999.0f;
     float kMaxValue = -99999999.0f;
 
-    for (int i = 0; i < _size; ++i)
+    for (int i = kStreamMaxSize - 1; i > kStreamMaxSize - 1 - _size; --i)
     {
         kMinValue = kMinValue < _data[i] ? kMinValue : _data[i];
         kMaxValue = kMaxValue > _data[i] ? kMaxValue : _data[i];
@@ -37,7 +35,7 @@ void Stream::draw(Allegro* allegro) const
     const float kScaleY = _renderArea.height() / (kMaxValue - kMinValue);
 
     Line line;
-    line._end = V2(_renderArea._min.x + (_size - 1) * _scaleX, _renderArea._max.y - (_data[_size - 1] - kMinValue) * kScaleY);
+    line._end = V2(_renderArea._min.x + (_size - 1) * _scaleX, _renderArea._max.y - (_data[_last] - kMinValue) * kScaleY);
 
     if (_size == 1)
     {
@@ -46,10 +44,11 @@ void Stream::draw(Allegro* allegro) const
         return;
     }
 
-    for (int i = _size - 1; i > 0; --i)
+    for (int i = 1; i < _size; ++i)
     {
         line._start = line._end;
-        line._end = V2(_renderArea._min.x + (i - 1) * _scaleX, _renderArea._max.y - (_data[i - 1] - kMinValue) * kScaleY);
+        line._end =
+            V2(_renderArea._min.x + (_size - 1 - i) * _scaleX, _renderArea._max.y - (_data[(_last + i) % kStreamMaxSize] - kMinValue) * kScaleY);
         Allegro::drawLine(line);
     }
 }
--- DeepRain/DeepRain/Source/Stream.h
+++ DeepRain/DeepRain/Source/Stream.h
@@ -14,6 +14,7 @@ class Stream
 
     float _data[kStreamMaxSize];
     int _size = 0;
+    int _last = kStreamMaxSize;
     char _name[kNameMaxSize] = "";
     const Rect _renderArea;
     const float _scaleX = 0.0f;
@@ -22,6 +23,7 @@ class Stream
     Stream(const char name[kNameMaxSize], const Rect& renderArea)
         : _renderArea(renderArea)
         , _size(0)
+        , _last(kStreamMaxSize)
         , _scaleX(_renderArea.width() / (kStreamMaxSize - 1))
     {
         strcpy_s(_name, name);
